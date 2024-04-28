a66d7bcd430fe2c2fadf320c9780c973b889ceb2
trandainien1 <trandainien1@gmail.com>
2024-04-28 19:23:55 +0700

    add model and combine ui with model params

--- ui.py
+++ ui.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from PIL import Image
-import tensorflow as tf
 import numpy as np
 import io
+import torch
+from diffusers import DiffusionPipeline
+from diffusers import StableDiffusionPipeline
 
 # Function to generate images based on user input
 def generate_images(text, size, num_images, quality):
@@ -28,28 +30,55 @@ st.title('Text-to-Image AI-based Chatbot')
 # Text input box
 user_input = st.text_input("Enter your text here:")
 
-# Image size input
-image_size_options = ["1080x1080", "1280x720", "1920x1080"]
-selected_size = st.selectbox("Select image size:", image_size_options)
+# Select models
+model_opts = ["amused/amused-256", "amused/amused-512", "runwayml/stable-diffusion-v1-5"]
+selected_model = st.selectbox("Select model:", model_opts)
+
+if selected_model == 'amused/amused-256':
+    print("Model selected: 256")
+    pipe = DiffusionPipeline.from_pretrained('amused/amused-256', variant="fp16", torch_dtype=torch.float16)
+elif selected_model == 'amused/amused-512':
+    print("Model selected: 512")
+    pipe = DiffusionPipeline.from_pretrained('amused/amused-512', variant="fp16", torch_dtype=torch.float16)
+else: 
+    print("Model selected: diffusion")
+    pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", variant="fp16", torch_dtype=torch.float16)
+
+# # Image size input
+# image_size_options = ["1080x1080", "1280x720", "1920x1080"]
+# selected_size = st.selectbox("Select image size:", image_size_options)
 
 # Convert selected size to width and height
-width, height = map(int, selected_size.split('x'))
+# width, height = map(int, selected_size.split('x'))
 
 # Number of images input
 num_images = st.number_input("Number of images to generate:", min_value=1, max_value=10, value=3, step=1)
-
 # Image quality input
-image_quality = st.slider("Select image quality:", min_value=1, max_value=100, value=80, step=5)
+image_quality = st.slider("Select image quality:", min_value=1, max_value=100, value=50, step=1)
 
 # Generate button
 if st.button("Generate Images"):
     if user_input:
+       
         # Example image
-        example_image = Image.open("example_img.jpg")
-        st.image(example_image, caption='Example Image', use_column_width=True)
-        img_bytes = pil_to_bytes(example_image)
-        st.download_button(label=f"Download Image ", data=img_bytes, file_name=f"generated_image_example.png", mime='image/png')
+        # example_image = Image.open("example_img.jpg")
+        # st.image(example_image, caption='Example Image', use_column_width=True)
+        # img_bytes = pil_to_bytes(example_image)
+        # st.download_button(label=f"Download Image ", data=img_bytes, file_name=f"generated_image_example.png", mime='image/png')
+        pipe = pipe.to('cuda')
+
+        with st.spinner('Generating...'):
+            image = pipe(
+                user_input, 
+                num_images_per_prompt=num_images, 
+                num_inference_steps=image_quality,
+                generator=torch.Generator('cuda').manual_seed(8)
+                ).images
+        st.image(image  , use_column_width=True)
+        # img_bytes = pil_to_bytes(image)
+        # st.download_button(label=f"Download Image ", data=img_bytes, file_name=f"generated_image_example.png", mime='image/png')
         
+
         # # Generate images based on user input
         # generated_images = generate_images(user_input, (width, height), num_images, image_quality)
         # if generated_images:
